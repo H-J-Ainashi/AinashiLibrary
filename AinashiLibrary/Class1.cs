41df0eade2aa7d108b09c66519b7538f01549d90
H.J.Ainashi@gmail.com <H.J.Ainashi@gmail.com>
2020-09-26 10:14:31 +0900

    @2020-09-26

--- AinashiLibrary/Class1.cs
+++ AinashiLibrary/Class1.cs
@@ -8,6 +8,7 @@ namespace AinashiLibraryCSharp
         // TODO: ICodeMapModuleインターフェースの作成(AinashiLibrary.ByteCodeMap.ICodeMapModule)
         // TODO: FileCodeMapModuleクラスの作成(AinashiLibrary.ByteCodeMap.FileCodeMapModule)
         // TODO: FileCodeMapNodeクラスの作成(AinashiLibrary.ByteCodeMap.FileCodeMapNode)
+        // TODO: 一部インターフェース実装の拡張メソッド化
 
     }
 }
